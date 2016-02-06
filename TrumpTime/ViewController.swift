3ba21ec1257002aa38f1f8d4675025afb4ae9110
manestay <bryanli.ca@gmail.com>
2016-02-06 10:01:33 -0500

    cool trump button

--- TrumpTime/ViewController.swift
+++ TrumpTime/ViewController.swift
@@ -53,6 +53,7 @@ class ViewController: UIViewController {
         return UIStatusBarStyle.LightContent
     }
     
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
