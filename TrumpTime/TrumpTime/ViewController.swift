b8d46b5b465bdaa36689f3bfaf27e364aab3d806
Jake Lee <louisoel@gmail.com>
2016-02-06 00:50:27 -0500

    UI updates, date functions

--- TrumpTime/TrumpTime/ViewController.swift
+++ TrumpTime/TrumpTime/ViewController.swift
@@ -10,16 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var theButton: UIButton!
+    
+    @IBOutlet weak var topLabel: UILabel!
+    
+    var pickedTime = NSDate()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        datePicker.datePickerMode = UIDatePickerMode.Time
+        let currentDate = NSDate()
+        datePicker.date = currentDate
+        print("Check1")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func myDateView(sender: UIDatePicker) {
+        setTime(datePicker.date)
+        print("TIME TRIGGERED" )
+    }
+    
+    @IBAction func myButton(sender: UIButton) {
+        print("BUTTON TRIGGERED" )
+        startTimer()
+    }
+    
+    func dateToString(aDate:NSDate) -> String{
+        let dateFormatter = NSDateFormatter()
+        let theDateFormat = NSDateFormatterStyle.NoStyle
+        let theTimeFormat = NSDateFormatterStyle.ShortStyle
+        
+        dateFormatter.dateStyle = theDateFormat
+        dateFormatter.timeStyle = theTimeFormat
+        
+        return dateFormatter.stringFromDate(pickedTime)
+    }
+    
+    func setTime(date:NSDate){
+        //set the alarm time
+        pickedTime = date
+    }
+    
+    func startTimer() {
+        //start the timer
+    }
+    
+    func playAlarm() {
+        //play alarm sound
+    }
+    
+    func stopAlarm() {
+        //stop alarm sound
+    }
+    
+    func snooze() {
+        
+    }
 
 }
 
